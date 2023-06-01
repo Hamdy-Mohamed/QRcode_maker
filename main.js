66f091b30f3ff04f2b5bd865c924361992a004fb
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-06-01 23:12:35 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ let img = document.querySelector("img"),
   api = "https://api.qrserver.com/v1/",
   api2 = "create-qr-code/?size=150x150&data=";
 
+// API
 btn.addEventListener("click", () => {
   img.src = `${api}${api2}${input.value}`;
 });
 
+// animation
 let oneSecond = "1000";
 let fewSeconds = oneSecond * 15;
 btn.addEventListener("mousemove", () => {
   setInterval(() => {
-    btn.style.animationName = "path";
     btn.innerHTML = "Contact";
     btn.style.color = "yellow";
   }, fewSeconds);
@@ -37,3 +38,26 @@ twitter.onclick = () => {
 linkedin.onclick = () => {
   window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/");
 };
+
+// animation
+
+function removeinput() {
+  input.onfocus = function () {
+    input.style.transition = "0.5s";
+    input.style.height = "3rem";
+    input.style.width = "15rem";
+  };
+  input.onblur = function () {
+    input.style.transition = "0.5s";
+    input.style.height = "1rem";
+    input.style.width = "1rem";
+  };
+}
+removeinput();
+
+setTimeout(() => {
+  let borning_img = document.querySelector(".borning_img");
+  window.addEventListener("load", function () {
+    borning_img.style.display = "block";
+  });
+}, 1000);
